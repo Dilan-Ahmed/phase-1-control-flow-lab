33e9f19f36e880381a626e507de8cc9df0263f2e
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-02-17 16:42:19 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  let offer;
+  if (distance <= 400) {
+    return (offer = "This one is on me!");
+  } else if (distance >= 400 && distance <= 2000) {
+    return (offer = "That will be twenty bucks.");
+  } else if (distance > 2000 && distance < 2500) {
+    return (offer = "I will gladly take your thirty bucks.");
+  } else if (distance > 2500) {
+    return (offer = "No can do.");
+  } else {
+    console.log("not within our range of service!");
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName) {
   // Write your code here!
+  return cityName === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charm) {
   // Write your code here!
+
+  switch (charm) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
